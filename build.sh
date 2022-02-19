2e1a35ba1cebe3587e137b9a46fb8f6045200645
Fabien Sanglier <fabien.sanglier@softwareaggov.com>
2022-02-19 16:09:26 -0500

    adding simple build

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## create build dir
+mkdir -p build/
+
+## package
+helm package -u -d build/ src/*
+
+## create new index with only the new modified charts
+helm repo index --merge docs/index.yaml build/
+
+## copy new index ot the final repo location
+cp -f build/index.yaml docs
+
+## clean up
+rm -Rf build/
